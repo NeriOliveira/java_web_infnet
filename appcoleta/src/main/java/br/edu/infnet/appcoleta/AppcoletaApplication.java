ba2ddc2a33e88a0421a370788eeaf927c9ce01a4
Patrick Neri de Oliveira <patrick.neri.oliveira@gmail.com>
2023-09-06 21:00:47 -0300

    Initial Springboot 2
    Segunda tentativa usando IDE Eclipse

---
+++ appcoleta/src/main/java/br/edu/infnet/appcoleta/AppcoletaApplication.java
@@ -0,0 +1,13 @@
+package br.edu.infnet.appcoleta;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppcoletaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppcoletaApplication.class, args);
+	}
+
+}
